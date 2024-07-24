a8628499f1684ab33a0ed3cac6a362a287d332c1
jcardus <asklocation.net@gmail.com>
2024-07-24 03:06:40 +0100

    Revert "add sentry"
    This reverts commit 6c1047992de859b75ca416f3aa77ddfdd3264005.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,6 @@ plugins {
     alias(libs.plugins.google.services)
     alias(libs.plugins.firebase.crashlytics)
     base
-
-    id("io.sentry.android.gradle") version "4.10.0"
 }
 
 base {
@@ -124,14 +122,4 @@ dependencies {
     "traccarImplementation"(libs.firebase.analytics)
     "traccarImplementation"(libs.firebase.crashlytics)
     "traccarImplementation"(libs.firebase.messaging)
-}
-
-
-sentry {
-    org.set("gateway-3s")
-    projectName.set("android")
-
-    // this will upload your source code to Sentry to show it as part of the stack traces
-    // disable if you don't want to expose your sources
-    includeSourceContext.set(true)
 }
